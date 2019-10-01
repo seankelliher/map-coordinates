0e2ccf1bad851a0dbece531520a533151c345bd6
Sean Kelliher <sbkelliher@icloud.com>
2019-09-30 22:20:57 -0400

    Add framework for JS functions

--- site.js
+++ site.js
@@ -1,1 +1,39 @@
-//Add JavaScript here.
+//Global variable (actually let)
+let convert;
+
+//Monitor page, after it loads invoke function.
+document.addEventListener("DOMContentLoaded", function () {
+    "use strict";
+
+    sample.functionOne();
+    sample.functionTwo();
+});
+
+//The convert variable (let)
+convert = {
+
+    functionOne: function () {
+        "use strict";
+
+        //add function content
+    },
+
+    functionTwo: function () {
+        "use strict";
+
+        //add function content
+    },
+
+    functionThree: function () {
+        "use strict";
+
+        //add function content
+    },
+
+    functionFour: function () {
+        "use strict";
+
+        //add function content
+    }
+    
+}; //close convert
