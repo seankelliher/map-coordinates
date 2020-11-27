bcc4634b973e6a00bac90bf08090766c71abac5d
Sean Kelliher <sbkelliher@icloud.com>
2020-11-27 14:52:52 -0500

    Add 'dd-to-dms-form.js' file.

---
+++ app/dd-to-dms-form.js
@@ -0,0 +1,35 @@
+//Show the DD to DMS form elements.
+function showDdToDmsForm () {
+    "use strict";
+
+    //Gather form elements.
+    const forms = document.querySelectorAll("form");
+    const formsArray = Array.from(forms); //Works without converting too.
+
+    //Show the selected forms. Hide the other.
+    formsArray.forEach(function (form) {
+        if (form.className === "dd-to-dms") {
+
+            //Show selected forms.
+            form.style.display = "block";
+
+            //Activate event listeners on forms.
+            //ddLatCheck();
+            //ddLonCheck();
+
+            //Get clicked tab. Add class to highlight it.
+            const ddOriginTab = document.getElementById("dd-to-dms");
+            ddOriginTab.classList.add("selected");
+        } else if (form.className === "dms-to-dd") {
+
+            //Hide "not selected" forms.
+            form.style.display = "none";
+
+            //Get "not clicked" tab. Remove class that highlights it.
+            const dmsOriginTab = document.getElementById("dms-to-dd");
+            dmsOriginTab.classList.remove("selected");
+        }
+    });
+}
+
+export { showDdToDmsForm };
