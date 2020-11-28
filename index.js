1be17359e1e14b2a787e0b311ce6c6b0a5a16d5b
Sean Kelliher <sbkelliher@icloud.com>
2020-11-28 16:06:16 -0500

    Shorten functions names. Add note.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { showDmsToDdForm } from "./app/dms-to-dd-form.js";
+import { showDmsForm } from "./app/form-dms.js";
 import { monitorTabs } from "./app/tabs.js";
 
 //Monitor page, after it loads invoke function.
@@ -6,8 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
     "use strict";
 
     //Display the "DMS to DD" tab.
-    showDmsToDdForm();
+    showDmsForm();
 
     //Monitor the tabs for clicks.
     monitorTabs();
 });
+
+//NOTE: When you see "DMS" or "DD" mentioned in a file name or function name,
+//it refers to the origin format. For example, "showDmsForm()" is the form that
+//converts coordinates from "DMS" to "DD".
