a9874bc974bc269cf689748abce73e615bd7aa04
Sean Kelliher <sbkelliher@icloud.com>
2020-11-29 15:28:10 -0500

    Replace 'placeholder' functions with the 'real' functions.

--- app/inputs-dd.js
+++ app/inputs-dd.js
@@ -8,20 +8,30 @@ function monitorBtnDdLat() {
 
     const ddLatButton = document.getElementById("dd-lat-btn");
 
-    ddLatButton.addEventListener("click", function (event) {
-        alert("DD to DMS Latitude Button");
-        event.stopImmediatePropagation();
+    ddLatButton.addEventListener("click", function () {
+        
+        const deciLat = document.getElementById("deci-lat").value;
+
+        const ddLatSum = document.getElementById("dd-lat-sum");
+
+        ddLatSum.textContent = integrityCheckDdLat(deciLat);
+
     });
 }
 
 function monitorBtnDdLon() {
     "use strict";
 
-    const ddLongButton = document.getElementById("dd-lon-btn");
+    const ddLonButton = document.getElementById("dd-lon-btn");
 
-    ddLongButton.addEventListener("click", function (event) {
-        alert("DD to DMS Longitude Button");
-        event.stopImmediatePropagation();
+    ddLonButton.addEventListener("click", function () {
+        
+        const deciLon = document.getElementById("deci-lon").value;
+
+        const ddLonSum = document.getElementById("dd-lon-sum");
+
+        ddLonSum.textContent = integrityCheckDdLon(deciLon);
+        
     });
 }
 
