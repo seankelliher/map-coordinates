b27f1bc7864f82b8cd76363838c36f627b64d96d
Sean Kelliher <sbkelliher@icloud.com>
2020-12-01 17:11:05 -0500

    Modify spacing, notes.

--- app/form-dd.js
+++ app/form-dd.js
@@ -1,36 +1,33 @@
-import {
-    monitorBtnDdLat,
-    monitorBtnDdLon
-} from "./inputs-dd.js";
+import {monitorBtnDdLat, monitorBtnDdLon} from "./inputs-dd.js";
 
 //Show the DD to DMS form elements.
 function showDdForm() {
     "use strict";
 
-    //Gather form elements.
+    //Gather all form elements.
     const forms = document.querySelectorAll("form");
     const formsArray = Array.from(forms); //Works without converting too.
 
-    //Show the selected forms. Hide the other.
+    //Show the DD to DMS form. Hide the other.
     formsArray.forEach(function (form) {
         if (form.className === "dd-to-dms") {
 
-            //Show selected forms.
+            //Show the DD to DMS form.
             form.style.display = "block";
 
-            //Activate event listeners on forms.
+            //Activate event listeners on form's "convert" buttons.
             monitorBtnDdLat();
             monitorBtnDdLon();
 
-            //Get clicked tab. Add class to highlight it.
+            //Get DD to DMS tab. Add class to highlight it.
             const ddTab = document.getElementById("dd-to-dms");
             ddTab.classList.add("selected");
         } else if (form.className === "dms-to-dd") {
 
-            //Hide "not selected" forms.
+            //Hide the DMS to DD form.
             form.style.display = "none";
 
-            //Get "not clicked" tab. Remove class that highlights it.
+            //Get DMS to DD tab. Remove class that highlights it.
             const dmsTab = document.getElementById("dms-to-dd");
             dmsTab.classList.remove("selected");
         }
