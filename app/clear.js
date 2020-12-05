8cd79b339dc0d2ae46577c194ad7fba8606ee3b7
Sean Kelliher <sbkelliher@icloud.com>
2020-12-05 00:30:42 -0500

    Modify earlier 'placeholder' function to working functions that clear inputs and 'sum'.

--- app/clear.js
+++ app/clear.js
@@ -1,54 +1,83 @@
+//Clear input fields. Used across forms.
+function clearInputs(form, sum) {
+    "use strict";
+
+    //Gather all inputs within selected form.
+    const inputs = form.querySelectorAll("input");
+
+    //Clear the inputs.
+    inputs.forEach(function (input) {
+        if (input.type === "text") {
+            input.value = "";
+        }
+        if (input.type === "radio") {
+            if (input.hasAttribute("checked") === true) {
+                input.checked = true;
+            } else {
+                input.checked = false;
+            }
+        }
+    });
+
+    //Get the selected sum. Clear it.
+    sum.textContent = "";
+}
+
+//Below functions are same, but for different forms.
+//Monitor the "clear" buttons in each form.
+//If clicked, send paramenters to the above function to clear inputs, text.
+
+//Monitor DMS to DD form - Latitude.
 function monitorDmsLatClear() {
     "use strict";
 
-    //Retrieve "clear" button.
     const dmsLatClear = document.getElementById("dms-lat-clear");
+    const dmsLatForm = document.getElementById("dms-lat-form");
+    const dmsLatSum = document.getElementById("dms-lat-sum");
 
-    //Add event listener. Clear inputs when clicked.
     dmsLatClear.addEventListener("click", function () {
-        //do something.
-        window.alert("clear button clicked");
+        clearInputs(dmsLatForm, dmsLatSum);
     });
 }
 
+//Monitor DMS to DD form - Longitude.
 function monitorDmsLonClear() {
     "use strict";
 
-    //Retrieve "clear" button.
     const dmsLonClear = document.getElementById("dms-lon-clear");
+    const dmsLonForm = document.getElementById("dms-lon-form");
+    const dmsLonSum = document.getElementById("dms-lon-sum");
 
-    //Add event listener. Clear inputs when clicked.
     dmsLonClear.addEventListener("click", function () {
-        //do something.
-        window.alert("clear button clicked");
+        clearInputs(dmsLonForm, dmsLonSum);
     });
 }
+
+//Monitor DD to DMS form - Latitude.
 function monitorDdLatClear() {
     "use strict";
 
-    //Retrieve "clear" button.
     const ddLatClear = document.getElementById("dd-lat-clear");
+    const ddLatForm = document.getElementById("dd-lat-form");
+    const ddLatSum = document.getElementById("dd-lat-sum");
 
-    //Add event listener. Clear inputs when clicked.
     ddLatClear.addEventListener("click", function () {
-        //do something.
-        window.alert("clear button clicked");
+        clearInputs(ddLatForm, ddLatSum);
     });
 }
 
+//Monitor DD to DMS form - Longitude.
 function monitorDdLonClear() {
     "use strict";
 
-    //Retrieve "clear" button.
     const ddLonClear = document.getElementById("dd-lon-clear");
+    const ddLonForm = document.getElementById("dd-lon-form");
+    const ddLonSum = document.getElementById("dd-lon-sum");
 
-    //Add event listener. Clear inputs when clicked.
     ddLonClear.addEventListener("click", function () {
-        //do something.
-        window.alert("clear button clicked");
+        clearInputs(ddLonForm, ddLonSum);
     });
 }
-
 
 export {
     monitorDmsLatClear,
